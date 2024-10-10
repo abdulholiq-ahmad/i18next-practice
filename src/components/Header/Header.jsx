2471a02f2ce3c601b23f85854c5bff9c80a95bb1
Ahmadbekov Abdulholiq <abdulholiq571@gmail.com>
2024-10-11 00:21:39 +0500

    add: language

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,20 @@ import { FaRegUser } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { IoCartOutline } from "react-icons/io5";
+import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [language, setLanguage] = useState("uz");
+
+  const { t, i18n } = useTranslation();
+  useEffect(
+    (e) => {
+      i18n.changeLanguage(language);
+    },
+    [language]
+  );
+
   return (
     <header className="w-full shadow-sm">
       <div className="container flex justify-between items-center py-4 ">
@@ -15,16 +27,16 @@ const Header = () => {
 
         <ul className="flex items-center gap-8 text-lg font-medium">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">{t("Header.home")}</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about">{t("Header.home")}</Link>
           </li>
           <li>
-            <Link to="/shop">Shop</Link>
+            <Link to="/shop">{t("Header.shop")}</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact">{t("Header.contact")}</Link>
           </li>
         </ul>
 
@@ -50,6 +62,10 @@ const Header = () => {
             </button>
           </li>
         </ul>
+        <select defaultValue={language} onChange={(e) => setLanguage(e.target.value)}>
+          <option value="uz">Uz</option>
+          <option value="eng">Eng</option>
+        </select>
       </div>
     </header>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,21 @@
 import heroBg from "../../assets/hero-bg.jpg";
+import { useTranslation } from "react-i18next";
 
 const Hero = () => {
+  const { t, i18n } = useTranslation();
   return (
     <section className="w-full relative">
-      <img src={heroBg} alt="" className="absolute top-0 left-2/4 -z-10 -translate-x-2/4 max-w-[1440px] h-[80vh] object-cover" />
-      <div className="container"></div>
+      <img src={heroBg} alt="" className="absolute top-0 left-2/4 -z-10 -translate-x-2/4 max-w-[1640px] h-[80vh] object-cover" />
+      <div className="container flex justify-end items-center h-[80vh] px-10">
+        <div className="flex justify-center items-center w-[643px] h-[443px] bg-[#FFF3E3]">
+          <div className="text-left p-5">
+            <p className="tracking-[3px] text-[#B88E2F]">{t("Home.hero.p1")}</p>
+            <h1 className="text-6xl font-bold mb-5 text-[#B88E2F]">{t("Home.hero.h1")}</h1>
+            <p className="text-xl mb-5">{t("Home.hero.lorem§")}</p>
+            <button className="p-5 px-16 bg-[#B88E2F] text-white">{t("Home.hero.btn")}</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
